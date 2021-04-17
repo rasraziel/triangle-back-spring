7a8b3bf780de5585a0118f7f62c74dad3196afd8
Pupkin <rasraziel@gmail.com>
2021-04-17 14:30:13 +0200

    Point slopes check added

--- src/main/java/com/elias/app/controller/TriangleController.java
+++ src/main/java/com/elias/app/controller/TriangleController.java
@@ -41,7 +41,7 @@ public class TriangleController {
         double bY = req.getB().getY();
         double cX = req.getC().getX();
         double cY = req.getC().getY();
-        return a.equals(b) || a.equals(c) || c.equals(b) || aX == bX && bX == cX && cX == aX|| aY == bY && bY == cY && cY == aY;
+        return a.equals(b) || a.equals(c) || c.equals(b) || aX == bX && bX == cX && cX == aX|| aY == bY && bY == cY && cY == aY || (bY-aY)/(bX-aX)==(cY-bY)/(cX-bX);
     }
 
     // Checking if sides are 0 length or if the sum of 2 sides is less or equal to the 3rd. An impossible triangle.
